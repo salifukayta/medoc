4db936541a5d1e666d23b712a180ffaabad2743a
salifukayta <solsalim@gmail.com>
2016-10-26 13:03:05 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	html/HomePage.html

--- js/soon.js
+++ js/soon.js
@@ -1,3 +1,26 @@
 /**
  * Created by Salifukayta on 26/10/2016.
  */
+/**
+ * Created by Salifukayta on 26/10/2016.
+ */
+function soon() {
+    document.getElementById('abc_soon').style.display = "block";
+}
+// Validating Empty Field
+// function check_empty() {
+//     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+//         alert("Fill All Fields !");
+//     } else {
+//         document.getElementById('form').submit();
+//         alert("Form Submitted Successfully...");
+//     }
+// }
+//Function To Display Popup
+function div_show_soon() {
+    document.getElementById('abc_soon').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide_soon(){
+    document.getElementById('abc_soon').style.display = "none";
+}
